0b7e9d0489d92ac0c53baecf25a4e9030000c165
mpnri <mpnri2831@gmail.com>
2023-05-31 02:29:08 +0330

    feat: Add crossover + mutation

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import math
 import bisect
 import random
 
-n = 8
+n = 16
+random.seed(42)
 
 
 class Creature:
@@ -11,21 +12,24 @@ class Creature:
             self.gens = [random.randint(1, n) for i in range(n)]
         else:
             self.gens = gens
-        # print(self.gens)
-        self._fitness = None
 
-    @property
-    def fitness(self) -> int:
-        if self._fitness is not None:
-            return self._fitness
+        # self._fitness = None
+        self.fitness = self.get_fitness()
+
+    # @property
+    # def fitness(self) -> int:
+    def get_fitness(self) -> int:
+        # if self._fitness is not None:
+        #     return self._fitness
         cnt = 0
         for i in range(n):
             for j in range(i + 1, n):
                 cnt += self.gens[i] != self.gens[j] and abs(i - j) != abs(
                     self.gens[i] - self.gens[j]
                 )
-        self._fitness = cnt
-        return self._fitness
+        # self._fitness = cnt
+        # return self._fitness
+        return cnt
 
     def show(self):
         for i in range(n):
@@ -36,24 +40,35 @@ class Creature:
                     print(".", end=" ")
             print()
 
+    def mutate(self):
+        # r = random.randint(0, n - 1)
+        # n_val = random.randint(1, n)
+        # self.gens[r] = n_val
+        r1 = random.randint(0, n - 1)
+        r2 = random.randint(0, n - 1)
+        self.gens[r1], self.gens[r2] = self.gens[r2], self.gens[r1]
 
-t = Creature()
-print(t.fitness)
-t.show()
-society_number = 10
+        # * self._fitness = None
+        self.fitness = self.get_fitness()
+
+    @staticmethod
+    def crossover(x: "Creature", y: "Creature"):
+        if random.random() < 0.99:
+            r = random.randint(0, n - 1)
+            return Creature(x.gens[:r] + y.gens[r:]), Creature(y.gens[:r] + x.gens[r:])
+        else:
+            return x, y
+
+
+society_number = 200
 maximum_fitness = (n * (n - 1)) // 2
 society = [Creature() for i in range(society_number)]
 
 generation_number = 0
-
 society.sort(key=lambda creature: creature.fitness, reverse=True)
 
-for i in range(10):
-    print(society[i].fitness)
-
-society[0].show()
-while society[0].fitness < maximum_fitness and generation_number < 100:
-    # todo: parent selection with child making
+while society[0].fitness < maximum_fitness and generation_number < 10_000:
+    # * roulette wheel selection
     accumulated_society_fitness = []
     tmp = 0
     for creature in society:
@@ -65,17 +80,32 @@ while society[0].fitness < maximum_fitness and generation_number < 100:
         ri1 = random.randint(0, total_fitness - 1)
         ri2 = random.randint(0, total_fitness - 1)
 
-        par1 = bisect.bisect_left(
-            accumulated_society_fitness, ri1, 0, len(accumulated_society_fitness)
-        )
-        par2 = bisect.bisect_left(
-            accumulated_society_fitness, ri1, 0, len(accumulated_society_fitness)
-        )
-        
+        par1 = bisect.bisect_left(accumulated_society_fitness, ri1, 0, society_number)
+        par2 = bisect.bisect_left(accumulated_society_fitness, ri2, 0, society_number)
+
         parent1 = society[par1]
         parent2 = society[par2]
-        #todo: make child
-        break
 
+        # * make child with crossover
+        child1, child2 = Creature.crossover(parent1, parent2)
+        new_society.append(child1)
+        new_society.append(child2)
+
+    for creature in new_society[1:]:
+        if random.random() < 0.1:
+            creature.mutate()
+
+    new_society.sort(key=lambda creature: creature.fitness, reverse=True)
+
+    society[society_number // 2 :] = new_society[: society_number // 2]
     society.sort(key=lambda creature: creature.fitness, reverse=True)
+    society = society[:society_number]
     generation_number += 1
+
+print()
+answer = society[0]
+if answer.fitness == maximum_fitness:
+    print("WIN")
+print(answer.fitness)
+print(answer.gens)
+answer.show()
