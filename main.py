d0efad03cc246569a271979c17c5da3e0e108847
mpnri <mpnri2831@gmail.com>
2023-05-30 18:01:10 +0330

    feat: Add parent selection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import bisect
 import random
 
 n = 8
@@ -11,17 +12,20 @@ class Creature:
         else:
             self.gens = gens
         # print(self.gens)
-        self._fitness = 0
+        self._fitness = None
 
     @property
-    def fitness(self) -> float:
+    def fitness(self) -> int:
+        if self._fitness is not None:
+            return self._fitness
         cnt = 0
         for i in range(n):
             for j in range(i + 1, n):
                 cnt += self.gens[i] != self.gens[j] and abs(i - j) != abs(
                     self.gens[i] - self.gens[j]
                 )
-        return cnt
+        self._fitness = cnt
+        return self._fitness
 
     def show(self):
         for i in range(n):
@@ -36,9 +40,9 @@ class Creature:
 t = Creature()
 print(t.fitness)
 t.show()
-society_first_number = 100
+society_number = 10
 maximum_fitness = (n * (n - 1)) // 2
-society = [Creature() for i in range(society_first_number)]
+society = [Creature() for i in range(society_number)]
 
 generation_number = 0
 
@@ -48,7 +52,30 @@ for i in range(10):
     print(society[i].fitness)
 
 society[0].show()
-while society[0].fitness == maximum_fitness or generation_number < 100:
+while society[0].fitness < maximum_fitness and generation_number < 100:
     # todo: parent selection with child making
+    accumulated_society_fitness = []
+    tmp = 0
+    for creature in society:
+        tmp += creature.fitness
+        accumulated_society_fitness.append(tmp)
+    total_fitness = tmp
+    new_society = []
+    while len(new_society) < society_number:
+        ri1 = random.randint(0, total_fitness - 1)
+        ri2 = random.randint(0, total_fitness - 1)
+
+        par1 = bisect.bisect_left(
+            accumulated_society_fitness, ri1, 0, len(accumulated_society_fitness)
+        )
+        par2 = bisect.bisect_left(
+            accumulated_society_fitness, ri1, 0, len(accumulated_society_fitness)
+        )
+        
+        parent1 = society[par1]
+        parent2 = society[par2]
+        #todo: make child
+        break
+
     society.sort(key=lambda creature: creature.fitness, reverse=True)
     generation_number += 1
